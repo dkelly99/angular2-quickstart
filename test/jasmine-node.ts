7cf3a20c4ffc5a85673000e18d5e297eb9cb8da9
Declan Kelly <declan.kelly@btinternet.com>
2016-06-23 21:04:04 +0100

    experiment

--- test/jasmine-node.ts
+++ test/jasmine-node.ts
@@ -5,10 +5,16 @@ const JASMINE_CONFIG_PATH = 'test/config/jasmine.json';
 
 let path = require('path'),
     Command = require('../node_modules/jasmine/lib/command.js'),
-    Jasmine = require('../node_modules/jasmine/lib/jasmine.js');
+    Jasmine = require('../node_modules/jasmine/lib/jasmine.js'),
+    ConsoleReporter = require('../node_modules/jasmine/lib/reporters/console_reporter.js');
 
 let jasmineInstance = new Jasmine({ projectBaseDir: path.resolve() });
 let examplesDir = path.join(__dirname, '..', 'node_modules', 'jasmine-core', 'lib', 'jasmine-core', 'example', 'node_example');
+
+let reporter = new ConsoleReporter();
+
+jasmineInstance.getEnv().addReporter(reporter);
+
 let command = new Command(path.resolve(), examplesDir, console.log);
 
 command.run(jasmineInstance, process.argv.slice(2).concat(['JASMINE_CONFIG_PATH='+JASMINE_CONFIG_PATH]));
